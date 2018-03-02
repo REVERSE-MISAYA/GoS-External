5e9e001e4aeb620fe0d77aae8b38504e1be4db58
REVERSE-MISAYA <36791157+REVERSE-MISAYA@users.noreply.github.com>
2018-03-02 05:02:58 +0100

    new issue

--- support/GoS_IssueTracker.lua
+++ support/GoS_IssueTracker.lua
@@ -13,3 +13,7 @@
 -- ISSUE: won't show the menu in game or load the Tick/FPS in top left.
 -- SOLUTION: MenuElement.Save file was deleted, and had to be re-added.
 
+-- 01/19/18 (goo.gl/ZHSRCN)
+-- ISSUE: Unable to use keyboard or mouse in-game.
+-- SOLUTION: Disable discord ingame overlays (found in overlay menu)
+
