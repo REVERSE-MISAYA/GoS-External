5d025c4854c0ed613b67444b1377d9134d502d3f
REVERSE-MISAYA <36791157+REVERSE-MISAYA@users.noreply.github.com>
2018-04-02 06:08:44 +0200

    update

---
+++ orbwalker/changelog.lua
@@ -0,0 +1,13 @@
+
+-- +--------------------------------------------------------------------+
+-- |               ReverseMisaya's Orbwalker: Changelog                 |
+-- +--------------------------------------------------------------------+
+-- |                                                                    |
+-- |  * Havent been keeping track of all previous changes :(            |
+-- |  * Release date? Soon™, I'm working on it whenever I can.          |
+-- |                                                                    |
+-- +--------------------------------------------------------------------+
+
+-- 02/04/18
+-- ADDED: Thresh AA-logic 
+-- NOTES: different from melee/ranged/missile champions, because of how his scythe works
