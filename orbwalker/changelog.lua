491a005c3ecb4cc79d432fd286bd5d2d1b38383b
REVERSE-MISAYA <36791157+REVERSE-MISAYA@users.noreply.github.com>
2018-04-03 06:06:10 +0200

    update

--- orbwalker/changelog.lua
+++ orbwalker/changelog.lua
@@ -5,14 +5,19 @@
 -- |                                                                                   |
 -- |              * Havent been keeping track of all previous changes :(               |
 -- |              * Release date? Soon™, I'm working on it whenever I can.             |
+-- |              * VIDEO #1: https://streamable.com/v4zjp                             |
 -- |                                                                                   |
 -- +-----------------------------------------------------------------------------------+
 
--- 01/04/18
--- ADDED: Updated minion damage calculations
--- NOTES: This needed a serious update from all the old shit people were using.
+-- 03/04/18
+-- ADDED: Extended all gameObject data
+-- NOTES: Additional entries added --> obj.trueRange, obj.isMoving, obj.sentAttack, etc
 
 -- 02/04/18
 -- ADDED: Thresh AA-logic 
 -- NOTES: Different from melee/ranged/missile champions, because of how his scythe works.
+
+-- 01/04/18
+-- ADDED: Updated minion damage calculations
+-- NOTES: This needed a serious update from all the old shit people were using.
 
